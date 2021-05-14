48b4d3a05c86827f2049496f6cdf2fbaf48c562c
Jatin <jatin.dhar27@gmail.com>
2021-05-14 23:18:11 +0530

    moving the turtle on screen with button using eventlisteners

---
+++ Turtle_libray/sketching.py
@@ -0,0 +1,36 @@
+import turtle as turtle_module
+
+tim=turtle_module.Turtle()
+screen = turtle_module.Screen()
+
+def move_forwards():
+	tim.forward(10)
+
+def move_backwards():
+	tim.backward(10)
+
+def move_counter_clockwise():
+	tim.circle(120,180)
+
+def left():
+	tim.left(45)
+
+def right():
+	tim.right(45)
+
+def clear():
+	tim.clear()
+	tim.penup()
+	tim.home()
+	tim.pendown()
+
+screen.listen()
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=move_counter_clockwise)
+screen.onkey(key="l", fun=left)
+screen.onkey(key="r", fun=right)
+screen.onkey(key="c", fun=clear)
+
+
+screen.exitonclick()
